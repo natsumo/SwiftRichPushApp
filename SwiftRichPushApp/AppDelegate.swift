68f1065cfad863e5803e448c6de3f26db0ef09f9
natsumo <ikeda.natsumo@fjct-8011.local>
2023-01-06 17:35:30 +0900

    プロジェクト及びREADMEの更新

--- SwiftRichPushApp/AppDelegate.swift
+++ SwiftRichPushApp/AppDelegate.swift
@@ -1,113 +1,88 @@
 //
 //  AppDelegate.swift
-//  SwiftPushApp_
+//  SwiftRichPushApp
 //
-//  Created by Ikeda Natsumo on 2016/12/08.
-//  Copyright © 2016年 NIFTY Corporation. All rights reserved.
+//  Created by ikeda.natsumo on 2023/01/06.
 //
 
 import UIKit
 import NCMB
 import UserNotifications
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
-    
-    var window: UIWindow?
-    
-    // APIキーの設定
-    let applicationkey = "YOUR_NCMB_APPLICATIONKEY"
-    let clientkey      = "YOUR_NCMB_CLIENTKEY"
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        // SDKの初期化
-        NCMB.setApplicationKey(applicationkey, clientKey: clientkey)
+        // APIキーの設定とSDK初期化
+        NCMB.initialize(applicationKey: "YOUR_APPLICATION_KEY", clientKey: "YOUR_CLIENT_KEY")
         
-        // デバイストークンの要求
-        if #available(iOS 10.0, *){
-            /** iOS10以上 **/
-            let center = UNUserNotificationCenter.current()
-            center.requestAuthorization(options: [.alert, .badge, .sound]) {granted, error in
-                if error != nil {
-                    // エラー時の処理
-                    return
-                }
-                if granted {
-                    // デバイストークンの要求
+        // APNsにデバイストークンを要求
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            if((error) != nil) {
+                return
+            }
+            if granted {
+                DispatchQueue.main.async {
                     UIApplication.shared.registerForRemoteNotifications()
                 }
             }
-        } else {
-            /** iOS8以上iOS10未満 **/
-            //通知のタイプを設定したsettingを用意
-            let setting = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            //通知のタイプを設定
-            application.registerUserNotificationSettings(setting)
-            //DevoceTokenを要求
-            application.registerForRemoteNotifications()
         }
         
-        // MARK: アプリが起動されるときに実行される処理を追記する場所
-        if let userInfo = launchOptions?[UIApplicationLaunchOptionsKey.remoteNotification] as! [AnyHashable : Any]! {
-            // リッチプッシュを表示させる処理
-            NCMBPush.handleRichPush(userInfo)
-            
+        // MARK: リッチプッシュ通知を表示させる
+        if let notification = launchOptions?[.remoteNotification] as? [String: AnyObject] {
+                NCMBPush.handleRichPush(userInfo: notification)
         }
         
         return true
     }
     
-    // デバイストークンが取得されたら呼び出されるメソッド
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        // 端末情報を扱うNCMBInstallationのインスタンスを作成
-        let installation : NCMBInstallation = NCMBInstallation.current()
-        // デバイストークンの設定
-        installation.setDeviceTokenFrom(deviceToken)
-        // 端末情報をデータストアに登録
-        installation.saveInBackground {error in
-            if error != nil {
-                // 端末情報の登録に失敗した時の処理
-                let err = error as! NSError
-                print("端末登録失敗：\(err.code)")
-            } else {
-                // 端末情報の登録に成功した時の処理
-                print("端末登録成功")
+        //端末情報を扱うNCMBInstallationのインスタンスを作成
+        let installation : NCMBInstallation = NCMBInstallation.currentInstallation
+
+        //Device Tokenを設定
+        installation.setDeviceTokenFromData(data: deviceToken)
+
+        //端末情報をデータストアに登録
+        installation.saveInBackground(callback: { result in
+            switch result {
+            case .success:
+                //端末情報の登録が成功した場合の処理
+                print("保存に成功しました")
+            case let .failure(error):
+                //端末情報の登録が失敗した場合の処理
+                print("保存に失敗しました: \(error)")
+                return;
             }
+        })
+    }
+    
+    // プッシュ通知を受信したときの処理
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],fetchCompletionHandler completionHandler:@escaping (UIBackgroundFetchResult) -> Void) {
+        if let notiData = userInfo as? [String : AnyObject] {
+            // MARK: リッチプッシュ通知を表示させる
+            NCMBPush.handleRichPush(userInfo: notiData)
         }
     }
-    
-    // MARK: アプリが起動しているときに実行される処理を追記する場所
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        // リッチプッシュを表示させる処理
-        NCMBPush.handleRichPush(userInfo)
-        
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-    
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
-    
+
+
 }
 
--- SwiftRichPushApp/ViewController.swift
+++ SwiftRichPushApp/ViewController.swift
@@ -2,8 +2,7 @@
 //  ViewController.swift
 //  SwiftRichPushApp
 //
-//  Created by Ikeda Natsumo on 2016/12/12.
-//  Copyright © 2016年 NIFTY Corporation. All rights reserved.
+//  Created by ikeda.natsumo on 2023/01/06.
 //
 
 import UIKit
@@ -12,12 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Do any additional setup after loading the view.
     }
 
 
